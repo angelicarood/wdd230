6dc083ae707be9fd7a8a7da436874fb662e67f45
angelicarood <leo18007@byui.edu>
2022-09-18 17:01:53 -0600

    change

---
+++ .js
@@ -0,0 +1,14 @@
+let daysnames =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sunday"];
+
+let monthsnames =["January", "February", "March", "April", "May",
+ "June", "July", "August", "September", "October", "November","December"];
+
+let d = new Date();
+
+let dayname = daysnames[d.getDay()];
+let monthname = monthsnames[d.getMonth()];
+let year = d.getFullYear();
+
+let fulldate = dayname + "," + monthname + "" + d.getDate() + "," + d.getFullYear();
+
+document.getElementById("currentdate").textContent = fulldate
